18cf4a445dedcec6000b1e90ed48844fa29fee5c
Student User <student@workstation.lab.example.com>
2023-03-17 13:35:06 -0400

    Fixed Thiago Cutrim

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\r\n\r\n";
-print "\r\n\t Thiago Cutrim";
+print "Hello, World! php version is " . PHP_VERSION . "\r\n\r\n" .  "\r\n\t Thiago Cutrim";
 ?>
